d936352a92a4d5e263d88845fca72e67ac7064f1
Nina Yao <ninayao@ninas-air.home>
2021-03-14 15:11:40 -0400

    countdown

--- typing game gui.py
+++ typing game gui.py
@@ -1,6 +1,14 @@
 import time
 import tkinter as tk
 
+def countdown(count):
+    # change text in label        
+    label['text'] = count
+
+    if count > 0:
+        # call countdown again after 1000ms (1s)
+        root.after(1000, countdown, count-1)
+
 root = tk.Tk()
 tk.Label(root, text="Text to Type:", bg="light blue").grid(row=0)
 tk.Label(root, text="Your text:", bg="light blue").grid(row=2)
@@ -14,5 +22,11 @@ root.configure(bg="light blue")
 textEntry = tk.Text(root, height=3, width=40).grid(row=2, column=1)
 submitBtn = tk.Button(root, text="submit").grid(row=10, column=1)
 
+label = tk.Label(root)
+label.place(x=35, y=15)
+countdown(10)
+
 
 root.mainloop()
+
+
