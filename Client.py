a0a6291abab7982e8b78a3cfd64ab62a3ba5fa89
Nina Yao <ninayao@ninas-air.home>
2021-04-03 15:28:08 -0400

    server

--- Client.py
+++ Client.py
@@ -2,6 +2,7 @@ import sys
 import time
 import string
 import os
+import socket, select
 
 class Game:
     text_dict = []
@@ -32,6 +33,7 @@ class Game:
 class Player:
     name = ""
     score = 0
+    powerup = ""
     # difficulty could modify how much the words are obscured at the start
     # difficulty  = 1
 
@@ -45,44 +47,61 @@ class Player:
         #and self.score >= 100\
         if power == "A":
             print("Changing mode to NONE for 5 seconds")
-            file = open("powerup.txt", "w")
-            file.write("A")
-            file.close()
-            self.score = self.score - 900
+            #file = open("powerup.txt", "w")
+            #file.write("A")
+            #file.close()
+            self.powerup = "A"
+            self.score = self.score - 100
         elif power == "B":
             print("Decreasing probabilty of flipped characters to 1/20 for 5 seconds")
-            file = open("powerup.txt", "w")
-            file.write("B") 
-            file.close()
-            self.score = self.score - 300
-        elif power == "C":
-            print("Slowing down text for 5 seconds")
-            file = open("powerup.txt", "w")
-            file.write("C")
-            file.close()
-            self.score = self.score - 600
+            #file = open("powerup.txt", "w")
+            #file.write("B") 
+            #file.close()
+            self.powerup = "B"
+            self.score = self.score - 100
         print(self.score)
     def clear_powerup(self):
-        open("powerup.txt", "w").close()
+        #open("powerup.txt", "w").close()
+        self.powerup = ""
 
+    '''
     def check_powerup(self):
         file = open("powerup.txt", "r")
         b = True
         if file.read() == "":
             b = False
         return b
+    '''
 
 
 if __name__ == '__main__':
 
+    host = 'localhost'
+    port = 8000
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+    
+    try:
+        s.connect((host, port))
+    except:
+        print('Connection error')
+        sys.exit()
+        
+    print('Connected to host')
+    #prompt()
+
+    #socket_list = [sys.stdin, s]
+
+    #read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
+    
     guess_number = 0
     username = input("Input username: \n")
     p1 = Player(username)
     print(p1.score)
     g1 = Game("sampletext.txt")
-    start_time = None
-    end_bool = open("powerup.txt", "r").read() == "END"
-    while(not end_bool):
+    #end_bool = open("powerup.txt", "r").read() == "END"
+    while True:
         user_input = input("Input index (starting at 0) followed by word guess or buy a powerup [A (900), B (300), C (600)]: \n")
         if user_input[0].isnumeric():
             result = g1.process_user_input(user_input, guess_number)
@@ -91,13 +110,13 @@ if __name__ == '__main__':
             guess_number += 1
             print(str(p1.score))
         else:
-            if p1.check_powerup():
-                print("Cannot use more than 1 powerup simultaneously\n")
-            elif p1.score < 100:
+            
+            if p1.score < 100:
                 print("Not enough points to buy a powerup\n")
             else:
+                s.send(user_input.encode())
                 p1.buy_powerup(user_input)
-        end_bool = open("powerup.txt", "r").read() == "END"
+        #end_bool = open("powerup.txt", "r").read() == "END"
         # else:
         #     print("Unacceptable input")
     print("Game over \nScore: ", p1.score)
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import sys
 import time
 from keylogger import Keylogger
 import os
+import socket, select, string
 
 
 #def keylog_from_text(text):
@@ -23,7 +24,6 @@ def print_text(speed, text):
         time.sleep(speed)
     return text
 
-
 if __name__ == '__main__':
 
     if len(sys.argv) > 1:
@@ -33,35 +33,45 @@ if __name__ == '__main__':
     k = Keylogger(mode)
     string_from_file = produce_text()
     speed = 0.2
+
+    host = 'localhost'
+    port = 8000
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+    
+    try:
+        s.connect((host, port))
+    except:
+        print('Connection error')
+        sys.exit()
+        
+    print('Connected to host')
+
     start = None
-    p_bool = False
-    #text = keylog_from_text(mode, string_from_file)
     for char in string_from_file:
-        file = open("powerup.txt", "r")
-        p_up = file.read()
-        if p_bool == False:
-            if p_up == "A" or p_up == "B" or p_up == "C":
-                p_bool = True
+        socket_list = [s, sys.stdin]
+        read_sockets, write, error = select.select(socket_list, [], [])
+        sock = read_sockets[0]
+        #print(sock)
+        if sock is not s:
+            c = k.simulated_key_pressed(char)
+            print_text(speed, c)
+            #print(speed)
+            print(k.get_mode())
+            #print(k.get_rand())
+        else :
+            data = sock.recv(4096)
+            if data:
+                p_up = data.decode()[-1]
                 start = time.time()
                 if p_up == "A":
-                    k.change_mode("none")  
+                    k.change_mode("none")
                 elif p_up == "B":
                     k.change_rand(20)
-                elif p_up == "C":
-                    speed = 0.5
-        end = time.time()    
+        end = time.time()
         if start is not None:
-            elapsed = end - start
-            if elapsed >= 5:
-                open("powerup.txt", "w").close()
+            if end - start >= 5:
                 k.change_mode(mode)
                 k.change_rand(10)
-                speed = 0.2    
-                p_bool = False        
-        c = k.simulated_key_pressed(char)
-        print_text(speed, c)
-        #print(speed)
-        #print(k.get_mode())
-        #print(k.get_rand())
-    file = open("powerup.txt", "w")
-    file.write("END")    
+                start = None
